8fbd75fe856ab59feaceaacbcaf431ef3d0e12b9
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-07-13 14:31:08 -0300

    Update IdiomaSistema.java

--- INICIO/IdiomaSistema.java
+++ INICIO/IdiomaSistema.java
@@ -1,1 +1,25 @@
-/*em breve*/
+/******************************************************************************
+ * DESCRIÇÃO:
+ * Programa simples que captura o idioma padrão configurado no sistema 
+ * operacional e exibe a informação no terminal do usuário.
+ *
+ * FUNCIONAMENTO:
+ * 1. Importa a classe Locale da biblioteca padrão do Java.
+ * 2. Consulta a máquina e armazena as configurações regionais do usuário.
+ * 3. Recupera o código de identificação do idioma local e o imprime na tela.
+ *
+ * Autor: Amanda-Aziz
+ * Data: 13 de Julho de 2026
+ *******************************************************************************/
+
+package com.mycompany.horadosistema;
+import java.util.Locale;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Locale linguagem = Locale.getDefault();
+        System.out.println("A linguagem do seu computador é: ");
+        System.out.print(linguagem);
+    }
+}
