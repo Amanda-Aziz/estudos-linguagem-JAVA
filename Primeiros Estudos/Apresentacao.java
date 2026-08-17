0ec818187a89f8e21199863e75a8620b389dede1
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-08-17 17:45:16 -0300

    apresentação em java

---
+++ Primeiros Estudos/Apresentacao.java
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * DESCRIÇÃO:
+ * Programa simples que captura informações pessoais do usuário, como nome,
+ * idade, peso e altura, e exibe os dados informados no terminal.
+ *
+ * FUNCIONAMENTO:
+ * 1. Importa a classe Scanner da biblioteca padrão do Java para permitir
+ *    a entrada de dados pelo teclado.
+ * 2. Cria o objeto 'entrada' para receber as informações digitadas pelo usuário.
+ * 3. Declara as variáveis 'nome', 'idade', 'peso' e 'altura' para armazenar
+ *    os dados informados.
+ * 4. Solicita ao usuário cada informação e armazena os valores nas variáveis.
+ * 5. Exibe todas as informações coletadas no terminal.
+ * 6. Fecha o objeto Scanner após o término da execução.
+ *
+ * Autor: Amanda-Aziz
+ * Data: 14 de Agosto de 2026
+ *******************************************************************************/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arguments){
+        Scanner entrada = new Scanner(System.in);
+
+        String nome;
+        int idade;
+        double peso;
+        double altura;
+        
+        System.out.print("\nDigite seu nome: ");
+        nome = entrada.nextLine(); //O nextLine() pega o texto que o usuário digitou e coloca dentro da variável X
+
+        System.out.print("Digite sua idade: ");
+        idade = entrada.nextInt(); //idade é um número inteiro, para isso usa-se nextInt()
+
+        System.out.print("Digite seu peso: ");
+        peso = entrada.nextDouble();
+
+        System.out.print("Digite sua altura: ");
+        altura = entrada.nextDouble();
+
+        System.out.println("\n===============================\n");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso + " kg");
+        System.out.println("Altura: " + altura + " m");
+
+        entrada.close();
+    }
+}
