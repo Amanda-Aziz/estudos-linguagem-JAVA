26ece6a94767b83d024141ab144398c4ac8c21e7
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-09-01 10:13:14 -0300

    Update EstudanteBolsista.java

--- EM-SALA/Aula5/EstudanteBolsista.java
+++ EM-SALA/Aula5/EstudanteBolsista.java
@@ -1,1 +1,22 @@
+public class EstudanteBolsista extends Estudante{
+    private double desconto;
 
+    //construtor
+    public EstudanteBolsista(String nome, int matricula, double mensalidade, double desconto){
+        super(nome, matricula, mensalidade);
+        this.desconto = desconto;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public double calcularMensalidade() {
+        double novamensalidade = getMensalidade() - (desconto*super.getMensalidade());
+        return novamensalidade;
+    }
+}
