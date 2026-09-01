ac12a09d17f4024d364cb669e883b5dc415311a2
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-09-01 10:12:45 -0300

    Update Main.java

--- EM-SALA/Aula5/Main.java
+++ EM-SALA/Aula5/Main.java
@@ -1,1 +1,27 @@
-....
+public class Main{
+
+    public static void main(String[] args) {
+        Estudante e = new Estudante("Pedro", 6767, 576.78);
+        e.entrarEmAula();
+        e.sairDeAula();
+
+        EstudanteBolsista eb = new EstudanteBolsista("Amanda", 4242, 576.78, 0.2);
+
+        eb.entrarEmAula();
+        eb.sairDeAula();
+
+        System.out.println("===================================");
+
+        System.out.println(e instanceof Estudante);
+        System.out.println(eb instanceof EstudanteBolsista);
+        System.out.println(e instanceof EstudanteBolsista);
+        System.out.println(eb instanceof Estudante);
+
+        System.out.println("===================================");
+
+        System.out.println("mensalidade do aluno " + e.getNome() + " e: " + e.getMensalidade());
+        System.out.println("mensalidade original do aluno " + eb.getNome() + " e: " + eb.calcularMensalidade());
+
+        System.out.println("===================================");
+    }
+}
